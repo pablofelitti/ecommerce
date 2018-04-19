b6b640a184b1ffdbeeb4cbcabec52d6704cf5035
Pablo Felitti <pablo.felitti@synapse.com>
2018-04-19 12:10:18 -0300

    added cart repository

---
+++ src/main/java/com/ecommerce/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.repository;
+
+import com.ecommerce.entity.Cart;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends CrudRepository<Cart, Long> {
+}
